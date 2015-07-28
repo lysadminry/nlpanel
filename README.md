ba2c32c15417ec38d039725c5c5ab1cf3ea25e68
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2015-07-28 23:11:06 +0300

    Add create_db.sql

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ nlpanel
 =======
 
 A _thing_ to (kind of) manage ldap users (not really, use at your own risk)
+Run ``sqlite3 path_to_db.sqlite < create_db.sql`` to initialise the database. 
---
+++ create_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE new_users (
+       username TEXT,
+       fname TEXT,
+       lname TEXT,
+       pname TEXT,
+       bday TEXT,
+       syear TEXT,
+       primary_group TEXT,
+       phone TEXT,
+       email TEXT
+);
