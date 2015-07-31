3eadb227f7a9fa2a28700106375f4df028bab0cf
Juho Rinta-Paavola <juho.s.rp@gmail.com>
2015-07-31 23:44:38 +0300

    Do preliminary work on cli backend

---
+++ bin/backend.py
@@ -0,0 +1,26 @@
+from pony.orm import *
+from configobj import ConfigObj
+
+dbConfig = ConfigObj('../etc/sql.conf')
+db = Database('sqlite', dbConfig['database'])
+
+class Application(db.Entity):
+    _table_='new_users'
+    username=Required(str)
+    fname=Required(str)
+    lname=Required(str)
+    pname=Required(str)
+    bday=Required(str)
+    syear=Required(str)
+    primary_group=Required(str)
+    phone=Optional(str)
+    email=Optional(str)
+
+db.generate_mapping()
+
+@db_session
+def main():
+    str = select(a for a in Application).show()
+    print str
+
+main()
